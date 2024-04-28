19d9f61dbd0bc97f62c98d31afeefdedf92a3567
muhammadTanver <Muhammadtanveertaqi@gmail.com>
2024-04-29 01:28:18 +0500

    first commit

---
+++ lec_1.js
@@ -0,0 +1,17 @@
+
+let lect_2= require("./lect_2");
+// var a = 4;
+// var b =6;
+// console.log(a+b);
+
+const arry  = [1,4,5,6];
+let result = arry.filter((item)=>{
+    return item >=4;
+    
+})
+console.log(result);
+ console.log(lect_2.a);
+ console.log(lect_2.x)
+
+
+ 
---
+++ lect_2.js
@@ -0,0 +1,8 @@
+module.exports ={
+    a:2,
+    b:3,
+    x:function(){
+        return 10;
+    }
+}
+
